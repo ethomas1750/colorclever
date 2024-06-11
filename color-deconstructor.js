af86bcf83d5b4c4869474398ba1667ce35291af1
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-10 23:03:38 -0500

    Update color-deconstructor.js

--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,1 +1,23 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+function colorDeconstructor(color1) {
+  if (color1 === "purple") {
+    return "red and blue"
+  } else if ( color1 === "green") {
+    return "blue and yellow"
+  } else if (color1 === "orange") {
+    return "red and yellow"
+  } else {
+    return "error"
+  }
+}
 
+
+// Our setup code here. Don't touch!
+if(typeof colorDeconstructor === 'undefined') {
+  colorDeconstructor = undefined;
+}
+
+
+module.exports = colorDeconstructor;
